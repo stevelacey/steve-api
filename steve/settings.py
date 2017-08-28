48635ace537e562088a7754bbe7c149186934159
Steve Lacey <steve@stevelacey.net>
2017-08-28 18:42:59 +0300

    Static and media

--- steve/settings.py
+++ steve/settings.py
@@ -128,7 +128,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+# Media files (user uploads)
 
-STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'web', 'uploads')
+MEDIA_URL = '/uploads/'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'web', 'assets')
+STATIC_URL = '/assets/'
